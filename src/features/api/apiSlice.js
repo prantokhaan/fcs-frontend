1da5a52a1da9937fe852f1ca60be0ed16b483bf3
unknown <prantokhaan@gmail.com>
2024-01-11 23:58:08 +0600

    something added

--- src/features/api/apiSlice.js
+++ src/features/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const apiSlice = createApi({
   reducerPath: "api",
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://fcs-server-o8a9.onrender.com/",
+    baseUrl: "http://localhost:8800",
   }),
   tagTypes: [
     "login",
